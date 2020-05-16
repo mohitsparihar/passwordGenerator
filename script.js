be8afd612939f795b4707d9bd5709202aefabe79
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-05-17 01:56:16 +0530

    second iteration

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-const characterAmountRange = document.getElementById('characterAmountRange');
-const characterAmountNumber = document.getElementById('characterAmountNumber');
-const includeUppercaseElement = document.getElementById('includeUppercase');
-const includeNumbersElement = document.getElementById('includeNumbers');
-const includeSymbolsElement = document.getElementById('includeSymbols');
-const form = document.getElementById('passwordGeneratorForm');
-const passwordDisplay = document.getElementById('passwordDisplay');
+(function () {
+    const app = {
+        init: function () {
+            this.cacheElements();
+            this.bindElements();
+        },
+        cacheElements: function () {
+            this.characterAmountRange = document.getElementById('characterAmountRange');
+            this.characterAmountNumber = document.getElementById('characterAmountNumber');
+            this.includeUppercaseElement = document.getElementById('includeUppercase');
+            this.includeNumbersElement = document.getElementById('includeNumbers');
+            this.includeSymbolsElement = document.getElementById('includeSymbols');
+            this.form = document.getElementById('passwordGeneratorForm');
+            this.passwordDisplay = document.getElementById('passwordDisplay');
 
-// to generate the password we are going to use the ASCII codes for different characters.
-const lowerCaseCharCodes = arrayFromLowToHigh(97, 122);
-const upperCaseCharCodes = arrayFromLowToHigh(65, 90);
-const numberCharCodes = arrayFromLowToHigh(48,57);
-const symbolCharCodes = arrayFromLowToHigh(33,47)
-        .concat(arrayFromLowToHigh(58,64))
-        .concat(arrayFromLowToHigh(91, 96))
-        .concat(arrayFromLowToHigh(123, 126));
-//symbols in ASCII characters are dispersed in a number of ranges so we used the .concat method to append all the symbols to a single array
+            this.lowerCaseCharCodes = this.range(97, 122);
+            this.upperCaseCharCodes = this.range(65, 90);
+            this.numberCharCodes = this.range(48, 57);
+            this.symbolCharCodes = this.range(33, 47)
+                .concat(this.range(58, 64))
+                .concat(this.range(91, 96))
+                .concat(this.range(123, 126));
+        },
+        bindElements: function () {
+            this.characterAmountNumber.addEventListener('input', this.syncCharacterAmount.bind(this));
+            this.characterAmountRange.addEventListener('input', this.syncCharacterAmount.bind(this));
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount);
-characterAmountRange.addEventListener('input', syncCharacterAmount);
+            //this .addEventListener is used for the password generator div  which takes the input from the checkboxes and the button
+            this.form.addEventListener('submit', event => {
+                event.preventDefault();
+                const characterAmount = this.characterAmountNumber.value;
+                const includeUppercase = this.includeUppercaseElement.checked;
+                const includeNumbers = this.includeNumbersElement.checked;
+                const includeSymbols = this.includeSymbolsElement.checked;
+                const password = this.generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+                this.passwordDisplay.innerText = password;
+            });
 
-//this .addEventListener is used for the password generator div  which takes the input from the checkboxes and the button
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const characterAmount = characterAmountNumber.value;
-    const includeUppercase = includeUppercaseElement.checked;
-    const includeNumbers = includeNumbersElement.checked;
-    const includeSymbols = includeSymbolsElement.checked;
-    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
-    passwordDisplay.innerText = password;
-});
+        },
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers,includeSymbols) {
-    let charCodes = lowerCaseCharCodes;//this assigns the charCodes to the lowercase default values
-    if(includeUppercase) {//returns true if the checkbox is checked
-        charCodes = charCodes.concat(upperCaseCharCodes);
+        generatePassword: function (characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+            const charCodesArray = [this.lowerCaseCharCodes]
+
+            // let charCodes = this.lowerCaseCharCodes;//this assigns the charCodes to the lowercase default values
+            if (includeUppercase) {//returns true if the checkbox is checked
+                charCodesArray.push(this.upperCaseCharCodes);
+            }
+            if (includeSymbols) {//returns true if the checkbox is checked
+                charCodesArray.push(this.symbolCharCodes);
+            }
+            if (includeNumbers) {//returns true if the checkbox is checked
+                charCodesArray.push(this.numberCharCodes);
+            }
+
+
+            const passwordCharacters = [];
+            //for loop to add the charCodes to the passwordCharacter array after generating them randomly
+            for (let i = 0, j = 0; i < characterAmount; i++) {
+                const characterCode = charCodesArray[j][Math.floor(Math.random() * charCodesArray[j].length)];
+                j++;
+                if (j >= charCodesArray.length) {
+                    j = 0;
+                }
+                passwordCharacters.push(String.fromCharCode(characterCode));
+            }
+            return _.shuffle(passwordCharacters).join('');
+        },
+
+        range: function (s,e) {
+            return _.range(s,e+1);
+        },
+
+        syncCharacterAmount: function (e) {
+            const value = e.target.value;
+            this.characterAmountNumber.value = value;
+            this.characterAmountRange.value = value;
+        }
     }
-    if(includeSymbols) {//returns true if the checkbox is checked
-        charCodes = charCodes.concat(symbolCharCodes);
-    }
-    if(includeNumbers) {//returns true if the checkbox is checked
-        charCodes = charCodes.concat(numberCharCodes);
-    }
-    const passwordCharacters =[];
-    //for loop to add the charCodes to the passwordCharacter array after generating them randomly
-    for (let i = 0; i < characterAmount; i++) {
-        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
-        passwordCharacters.push(String.fromCharCode(characterCode));
-    }
-    return passwordCharacters.join('');
-}
 
-function arrayFromLowToHigh(low, high) {
-    const array = [];
-    for(let i = low; i <= high; i++) {
-        array.push(i);
-    }
-    return array;
-}
+    app.init();
+}());
 
-function syncCharacterAmount(e) {
-    const value = e.target.value;
-    characterAmountNumber.value = value;
-    characterAmountRange.value = value;
-}
 
